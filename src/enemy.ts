da329becce7e5b031224ffd9e7e982895ef716d4
Yoshioka Ryoto <liangrenjigang@gmail.com>
2026-06-22 23:19:27 +0900

    enemy.taを追加し、必要なコードを書いた

---
+++ src/enemy.ts
@@ -0,0 +1,35 @@
+import { Character } from "./character";
+
+export class Enemy extends Character {
+  //TODO: maxHp プロパティを追加する
+  private maxHP: number;
+  //TODO: maxHp プロパティの初期化
+  constructor(name: string, hp: number) {
+    super(name, hp);
+    this.maxHP = hp;
+  }
+
+  //TODO: maxHp に対する現在の hp の割合を返す getHpRatio メソッドを追加する
+  getHpRatio(): number {
+    return this.hp / this.maxHP;
+  }
+
+  //TODO: ダメージを受ける takeDamage メソッドを追加する
+  takeDamage(damage: number): void {
+    this.hp -= damage;
+    if (this.hp < 0) {
+      this.hp = 0;
+    }
+  }
+
+  //TODO: 残り HP が 30% 以下の場合、行動を変化させる
+  // 通常時 : name + "は攻撃してきた！"
+  // 残り HP が 30% 以下 : name + "は必死に抵抗している！"
+  attack(): void {
+    if (this.getHpRatio() > 0.3) {
+      console.log(`${this.name}は攻撃してきた！`);
+    } else {
+      console.log(`${this.name}は必死に抵抗している！`);
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,12 @@ warrior.attack();
 const mage = new Mage("メディア", 80);
 mage.showStatus();
 mage.attack();
+
+import { Enemy } from "./enemy";
+
+const slime = new Enemy("スライム", 50);
+slime.showStatus();
+slime.attack();
+slime.takeDamage(40);
+slime.showStatus();
+slime.attack();
